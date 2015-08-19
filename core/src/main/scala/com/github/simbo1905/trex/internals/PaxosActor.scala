7bfb8917a2e45c2c7a59e71179fb22451cc76443
simbo1905 <simbo1905@60hertz.com>
2015-08-19 21:04:41 +0100

    split out paxos data into a seperate file and mended some scaladoc

--- core/src/main/scala/com/github/simbo1905/trex/internals/PaxosActor.scala
+++ core/src/main/scala/com/github/simbo1905/trex/internals/PaxosActor.scala
@@ -25,7 +25,8 @@ object Ordering {
 }
 
 /**
- * Paxos Actor Finite State Machine using immutable messages and immutable state.
+ * Paxos Actor Finite State Machine using immutable messages and immutable state. Note that for testing this class does
+ * not schedule and manage its own timeouts. Extend a subclass which schedules its timeout rather than this baseclass.
  *
  * @param config Configuration such as timeout durations and the cluster size.
  * @param nodeUniqueId The unique identifier of this node. This *must* be unique in the cluster which is required as of the Paxos algorithm to work properly and be safe.
@@ -37,8 +38,8 @@ with RetransmitHandler
 with ReturnToFollowerHandler
 with UnhandledHandler
 with CommitHandler
-with ResendAcceptsHandler
-{
+with ResendAcceptsHandler {
+
   import Ordering._
 
   val minPrepare = Prepare(Identifier(nodeUniqueId, BallotNumber(Int.MinValue, Int.MinValue), Long.MinValue))
@@ -472,7 +473,7 @@ with ResendAcceptsHandler
             val updated = oldData.acceptResponses + (vote.requestId -> AcceptResponsesAndTimeout(randomTimeout, accept, Map.empty))
 
             // grab all the accepted values from the beginning of the tree map
-            val (committable, uncommittable) = updated.span { case (_, AcceptResponsesAndTimeout(_,_,rs)) => rs.isEmpty }
+            val (committable, uncommittable) = updated.span { case (_, AcceptResponsesAndTimeout(_, _, rs)) => rs.isEmpty }
             log.debug("Node " + nodeUniqueId + " {} vote {} committable {} uncommittable {}", stateName, vote, committable, uncommittable)
 
             // this will have dropped the committable 
@@ -506,8 +507,8 @@ with ResendAcceptsHandler
                 log.debug("Node {} {} post commit has responds.size={}, remainders.size={}", nodeUniqueId, stateName, responds.size, remainders.size)
                 results foreach { case (id, bytes) =>
                   responds.get(id) foreach { case (cmd, client) =>
-                      log.debug("sending response from accept {} to {}", id, client)
-                      send(client, bytes)
+                    log.debug("sending response from accept {} to {}", id, client)
+                    send(client, bytes)
                   }
                 }
                 // FIXME memory leak we have not GCed the stuff we are no longer awaiting responses in votesData.acceptResponses
@@ -592,7 +593,7 @@ with ResendAcceptsHandler
           // self accept
           journal.accept(accept)
           // register self
-          val updated = data.acceptResponses + (aid -> AcceptResponsesAndTimeout(randomTimeout,accept,Map(nodeUniqueId -> AcceptAck(aid, nodeUniqueId, data.progress))))
+          val updated = data.acceptResponses + (aid -> AcceptResponsesAndTimeout(randomTimeout, accept, Map(nodeUniqueId -> AcceptAck(aid, nodeUniqueId, data.progress))))
           // broadcast
           broadcast(accept)
           // add the sender our client map
@@ -743,126 +744,6 @@ abstract class PaxosActorWithTimeout(config: Configuration, nodeUniqueId: Int, b
  * @param responses The known responses.
  */
 case class AcceptResponsesAndTimeout(timeout: Long, accept: Accept, responses: Map[Int, AcceptResponse])
-
-/**
- * We use a stateless Actor FSM pattern. This immutable case class holds the state of a node in the cluster.
- * Note that for testing this class does not schedule and manage its own timeouts. Use the subclass which
- * schedules its timeout rather than this baseclass.
- *
- * @param progress The highest promised and highest committed progress of a node in the cluster.
- * @param leaderHeartbeat The last heartbeat value seen from a leader. Note that clocks are not synced so this value is only used as evidence that a stable leader is up whereas the paxos number and committed slot are taken as authoritative that a new leader is making progress.
- * @param timeout The next randomised point in time that this node will timeout. Followers timeout on Commit messages and become a Recoverer. Recoverers timeout on PrepareResponses and AcceptResponses. Leaders timeout on AcceptResponses.
- * @param clusterSize The current size of the cluster.
- * @param prepareResponses The outstanding uncommitted proposed work of the leader take over phase during the recovery of a leader failover.
- *                         Each key is an identifier of a prepare for which we are collecting a majority response to determine the highest proposed value of the previous leader if any.
- * @param epoch The leaders paxos number when leading.
- * @param acceptResponses Tracking of responses to accept messages when Recoverer or Leader. Each key is an identifier of the command we want to commit. Each value is a map of the ack/nacks of each cluster node with a timeout. 
- * @param clientCommands The client work outstanding with the leader. The map key is the accept identifier and the value is a tuple of the client command and the client ref.
- */
-case class PaxosData(progress: Progress,
-                     leaderHeartbeat: Long,
-                     timeout: Long,
-                     clusterSize: Int,
-                     prepareResponses: SortedMap[Identifier, Option[Map[Int, PrepareResponse]]] = SortedMap.empty[Identifier, Option[Map[Int, PrepareResponse]]](Ordering.IdentifierLogOrdering),
-                     epoch: Option[BallotNumber] = None,
-                     acceptResponses: SortedMap[Identifier, AcceptResponsesAndTimeout] = SortedMap.empty[Identifier, AcceptResponsesAndTimeout](Ordering.IdentifierLogOrdering),
-                     clientCommands: Map[Identifier, (CommandValue, ActorRef)] = Map.empty)
-
-object PaxosData {
-  val prepareResponsesLens = Lens(
-    get = (_: PaxosData).prepareResponses,
-    set = (nodeData: PaxosData, prepareResponses: SortedMap[Identifier, Option[Map[Int, PrepareResponse]]]) => nodeData.copy(prepareResponses = prepareResponses))
-
-  val acceptResponsesLens = Lens(
-    get = (_: PaxosData).acceptResponses,
-    set = (nodeData: PaxosData, acceptResponses: SortedMap[Identifier, AcceptResponsesAndTimeout]) => nodeData.copy(acceptResponses = acceptResponses)
-  )
-
-  val clientCommandsLens = Lens(
-    get = (_: PaxosData).clientCommands,
-    set = (nodeData: PaxosData, clientCommands: Map[Identifier, (CommandValue, ActorRef)]) => nodeData.copy(clientCommands = clientCommands)
-  )
-
-  val acceptResponsesClientCommandsLens = Lens(
-    get = (n: PaxosData) => ((acceptResponsesLens(n), clientCommandsLens(n))),
-    set = (n: PaxosData, value: (SortedMap[Identifier, AcceptResponsesAndTimeout], Map[Identifier, (CommandValue, ActorRef)])) =>
-      value match {
-        case (acceptResponses: SortedMap[Identifier, AcceptResponsesAndTimeout], clientCommands: Map[Identifier, (CommandValue, ActorRef)]) =>
-          acceptResponsesLens.set(clientCommandsLens.set(n, clientCommands), acceptResponses)
-      }
-  )
-
-  val timeoutLens = Lens(
-    get = (_: PaxosData).timeout,
-    set = (nodeData: PaxosData, timeout: Long) => nodeData.copy(timeout = timeout)
-  )
-
-  val epochLens = Lens(
-    get = (_: PaxosData).epoch,
-    set = (nodeData: PaxosData, epoch: Option[BallotNumber]) => nodeData.copy(epoch = epoch)
-  )
-
-  val leaderLens = Lens(
-    get = (nodeData: PaxosData) => ((prepareResponsesLens(nodeData), acceptResponsesLens(nodeData), clientCommandsLens(nodeData))),
-    set = (nodeData: PaxosData, value: (SortedMap[Identifier, Option[Map[Int, PrepareResponse]]],
-      SortedMap[Identifier, AcceptResponsesAndTimeout],
-      Map[Identifier, (CommandValue, ActorRef)])) =>
-      value match {
-        case (prepareResponses: SortedMap[Identifier, Option[Map[Int, PrepareResponse]]],
-        acceptResponses: SortedMap[Identifier, AcceptResponsesAndTimeout],
-        clientCommands: Map[Identifier, (CommandValue, ActorRef)]) =>
-          prepareResponsesLens.set(acceptResponsesLens.set(clientCommandsLens.set(nodeData, clientCommands), acceptResponses), prepareResponses)
-      }
-  )
-
-  val backdownLens = Lens(
-    get = (n: PaxosData) => ((prepareResponsesLens(n), acceptResponsesLens(n), clientCommandsLens(n), epochLens(n), timeoutLens(n))),
-    set = (n: PaxosData, value: (SortedMap[Identifier, Option[Map[Int, PrepareResponse]]], SortedMap[Identifier, AcceptResponsesAndTimeout], Map[Identifier, (CommandValue, ActorRef)], Option[BallotNumber], Long)) =>
-      value match {
-        case
-          (prepareResponses: SortedMap[Identifier, Option[Map[Int, PrepareResponse]]],
-          acceptResponses: SortedMap[Identifier, AcceptResponsesAndTimeout],
-          clientCommands: Map[Identifier, (CommandValue, ActorRef)],
-          epoch: Option[BallotNumber],
-          timeout: Long
-            ) => prepareResponsesLens.set(acceptResponsesLens.set(clientCommandsLens.set(epochLens.set(timeoutLens.set(n, timeout), epoch), clientCommands), acceptResponses), prepareResponses)
-      }
-  )
-
-  val progressLens = Lens(
-    get = (_: PaxosData).progress,
-    set = (nodeData: PaxosData, progress: Progress) => nodeData.copy(progress = progress)
-  )
-
-  val highestPromisedLens = progressLens andThen Lens(get = (_: Progress).highestPromised, set = (progress: Progress, promise: BallotNumber) => progress.copy(highestPromised = promise))
-
-  val timeoutPrepareResponsesLens = Lens(
-    get = (nodeData: PaxosData) => ((timeoutLens(nodeData), prepareResponsesLens(nodeData))),
-    set = (nodeData: PaxosData, value: (Long, SortedMap[Identifier, Option[Map[Int, PrepareResponse]]])) =>
-      value match {
-        case (timeout: Long, prepareResponses: SortedMap[Identifier, Option[Map[Int, PrepareResponse]]]) =>
-          timeoutLens.set(prepareResponsesLens.set(nodeData, prepareResponses: SortedMap[Identifier, Option[Map[Int, PrepareResponse]]]), timeout)
-      }
-  )
-
-  val progressAcceptResponsesEpochTimeoutLens = Lens(
-    get = (nodeData: PaxosData) => ((progressLens(nodeData), acceptResponsesLens(nodeData), epochLens(nodeData), timeoutLens(nodeData))),
-    set = (nodeData: PaxosData, value: (Progress, SortedMap[Identifier, AcceptResponsesAndTimeout], Option[BallotNumber], Long)) =>
-      value match {
-        case (progress: Progress, acceptResponses: SortedMap[Identifier, AcceptResponsesAndTimeout], epoch: Option[BallotNumber], timeout: Long) =>
-          acceptResponsesLens.set(timeoutLens.set(epochLens.set(progressLens.set(nodeData, progress), epoch), timeout), acceptResponses)
-      }
-  )
-
-  val highestPromisedTimeoutEpochPrepareResponsesAcceptResponseLens = Lens(
-    get = (n: PaxosData) => (highestPromisedLens(n), timeoutLens(n), epochLens(n), prepareResponsesLens(n), acceptResponsesLens(n)),
-    set = (n: PaxosData, value: (BallotNumber, Long, Option[BallotNumber], SortedMap[Identifier, Option[Map[Int, PrepareResponse]]], SortedMap[Identifier, AcceptResponsesAndTimeout])) =>
-      value match {
-        case (promise: BallotNumber, timeout: Long, epoch: Option[BallotNumber], prepareResponses: SortedMap[Identifier, Option[Map[Int, PrepareResponse]]], acceptResponses: SortedMap[Identifier, AcceptResponsesAndTimeout]) =>
-          highestPromisedLens.set(timeoutLens.set(epochLens.set(prepareResponsesLens.set(acceptResponsesLens.set(n, acceptResponses), prepareResponses), epoch), timeout), promise)
-      }
-  )
-}
 
 object PaxosActor {
 
