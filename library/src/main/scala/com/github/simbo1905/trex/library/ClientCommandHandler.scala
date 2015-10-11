d7421b76dddbf3661615e44bda7cdde123d2491f
simbo1905 <simbo1905@60hertz.com>
2015-10-11 09:33:06 +0100

    more test coverage around client command handling and fix to issue #20

--- library/src/main/scala/com/github/simbo1905/trex/library/ClientCommandHandler.scala
+++ library/src/main/scala/com/github/simbo1905/trex/library/ClientCommandHandler.scala
@@ -3,35 +3,47 @@ package com.github.simbo1905.trex.library
 import scala.collection.immutable.SortedMap
 import Ordering._
 
-// FIXME no test coverage in library project
 trait ClientCommandHandler[RemoteRef] extends PaxosLenses[RemoteRef] {
-  def handleClientCommand(io: PaxosIO[RemoteRef], agent: PaxosAgent[RemoteRef], value: CommandValue, client: RemoteRef): PaxosAgent[RemoteRef] = {
-    agent.data.epoch match {
-      // the following 'if' check is an invariant of the algorithm we will throw and kill the actor if we have no match
-      case Some(epoch) if agent.data.progress.highestPromised <= epoch =>
-        // compute next slot
-        val lastLogIndex: Long = agent.data.acceptResponses.lastOption match {
-          case Some((id, _)) => id.logIndex
-          case _ => agent.data.progress.highestCommitted.logIndex
-        }
-        // create accept
-        val nextLogIndex = lastLogIndex + 1
-        val aid = Identifier(agent.nodeUniqueId, agent.data.epoch.get, nextLogIndex)
-        val accept = Accept(aid, value)
 
-        // self accept
-        io.journal.accept(accept)
-        // register self
-        val updated = agent.data.acceptResponses + (aid -> AcceptResponsesAndTimeout(io.randomTimeout, accept,
-          Map(agent.nodeUniqueId -> AcceptAck(aid, agent.nodeUniqueId, agent.data.progress))))
-        // broadcast
-        io.send(accept)
-        // add the sender our client map
-        val clients = agent.data.clientCommands + (accept.id ->(value, client))
-        agent.copy(data = leaderLens.set(agent.data, (SortedMap.empty, updated, clients)))
-      case x =>
-        throw new AssertionError(s"Invariant violation as '$x' does not match case Some(epoch) if ${agent.data.progress.highestPromised} <= epoch")
+  import ClientCommandHandler._
+
+  def handleClientCommand(io: PaxosIO[RemoteRef], agent: PaxosAgent[RemoteRef], value: CommandValue, client: RemoteRef): PaxosAgent[RemoteRef] = {
+    val accept = acceptFor(agent, value)
+    val SelfAckOrNack(response, updated) = leaderSelfAckOrNack(io, agent, accept)
+    response match {
+      case _: AcceptAck => io.journal.accept(accept)
+      case _ => // do nothing
+    }
+    // respond
+    io.send(accept)
+    // add the sender our client map
+    val clients = agent.data.clientCommands + (accept.id ->(value, client))
+    agent.copy(data = leaderLens.set(agent.data, (SortedMap.empty, updated, clients)))
+  }
+}
+
+case class SelfAckOrNack(response: AcceptResponse, acceptResponses: SortedMap[Identifier, AcceptResponsesAndTimeout])
+
+object ClientCommandHandler {
+  def acceptFor[RemoteRef](agent: PaxosAgent[RemoteRef], value: CommandValue): Accept = {
+    // compute next slot
+    val lastLogIndex: Long = agent.data.acceptResponses.lastOption match {
+      case Some((id, _)) => id.logIndex
+      case _ => agent.data.progress.highestCommitted.logIndex
+    }
+    // create accept
+    val nextLogIndex = lastLogIndex + 1
+    val aid = Identifier(agent.nodeUniqueId, agent.data.epoch.get, nextLogIndex)
+    Accept(aid, value)
+  }
+
+  def leaderSelfAckOrNack[RemoteRef](io: PaxosIO[RemoteRef], agent: PaxosAgent[RemoteRef], accept: Accept): SelfAckOrNack = {
+    val response = agent.data.progress.highestPromised match {
+      case promise if promise > accept.id.number => AcceptNack(accept.id, agent.nodeUniqueId, agent.data.progress)
+      case _ => AcceptAck(accept.id, agent.nodeUniqueId, agent.data.progress)
     }
 
+    SelfAckOrNack(response, agent.data.acceptResponses + (accept.id -> AcceptResponsesAndTimeout(io.randomTimeout, accept,
+      Map(agent.nodeUniqueId -> response))))
   }
 }
