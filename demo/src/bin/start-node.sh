da6783d012e9f6057eb2198fe03ec48fa91ce3ea
root <root@n3>
2015-12-19 22:37:52 +0000

    changes to launch scripts for jepsen lxc nodes

--- demo/src/bin/start-node.sh
+++ demo/src/bin/start-node.sh
@@ -1,1 +1,9 @@
-java -cp ./demo/src/main/resources:./demo/target/scala-2.11/trex-demo-assembly-0.1.jar com.github.trex_paxos.demo.TrexKVStore $* | tee /tmp/trex.$$
+CONFIG=$1
+echo "CONFIG=$CONFIG"
+NODE=$2
+echo "NODE=$NODE"
+NODE_NUM=$(echo $NODE | sed 's/n//1' )
+echo "$NODE_NUM=NODE_NUM"
+mkdir -p /tmp/$NODE_NUM
+java -cp ./demo/src/main/resources:./demo/target/scala-2.11/trex-demo-assembly-0.1.jar com.github.trex_paxos.demo.TrexKVStore $CONFIG $NODE_NUM | tee /tmp/trex.$$
+
